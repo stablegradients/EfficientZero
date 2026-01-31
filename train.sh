83b5e20995797e983551fb22dea8a7b956fb9cd7
stablegradients <stablegradients@icloud.com>
2026-01-31 16:12:14 -0500

    train params for lab compute

--- train.sh
+++ train.sh
@@ -1,12 +1,16 @@
 set -ex
+rm -rf /tmp/ray/session_* 2>/dev/null || true
+export RAY_OBJECT_SPILLING_CONFIG='{"type":"filesystem","params":{"directory_path":"/home/scratch/shrinivr"}}'
 export CUDA_DEVICE_ORDER='PCI_BUS_ID'
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=2,3
 
 python main.py --env BreakoutNoFrameskip-v4 --case atari --opr train --force \
-  --num_gpus 4 --num_cpus 96 --cpu_actor 14 --gpu_actor 20 \
+  --num_gpus 2 --num_cpus 32 --cpu_actor 8 --gpu_actor 8 \
   --seed 0 \
   --p_mcts_num 4 \
   --use_priority \
   --use_max_priority \
   --amp_type 'torch_amp' \
-  --info 'EfficientZero-V1'
+  --info 'EfficientZero-V1'\
+  --object_store_memory 100000000000 \
+  --debug
